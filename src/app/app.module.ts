db76f9649b5440e1f4198be546d6dbff3da62525
LuanRLima <luanrlima9@gmail.com>
2021-12-07 08:06:24 -0300

    Class 6 creating component card and  new functionalities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CardsComponent } from './components/cards/cards.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Finance} from '../../models/finance';
 
 @Component({
   selector: 'app-home',
@@ -7,60 +8,150 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  //cards
   valorTotal: number = 0;
   saida: number = 0;
   entrada: number = 0;
   investimentos: number = 0;
+  financeModel = {
+    tipo: 'Entrada',
+    valor: 888,
+    data: '12/01/2020'
+  }
 
+  //objeto da model
+  financa: Finance ={
+    tipo: 'Entrada',
+    valor: 888,
+    data: '12/01/2020'
+  }
+  //array de obejto da model
+  financas: Finance[] = [];
 
-  finances = [
-    {tipo: 'Saída',
-    valor:  100,
-    data: '01/01/2020'
-    },
-    {tipo: 'Entrada',
-    valor:  100,
-    data: '01/01/2020'
-    },
-    {tipo: 'Saída',
-    valor:  5000,
-    data: '01/01/2020'
-    },
-    {tipo: 'Investimento',
-    valor:  2000,
-    data: '01/01/2020'
-    },
-    {tipo: 'Saída',
-    valor:  1010,
-    data: '01/01/2020'
-    },
-
-  ]
+  finance = [{
+    tipo: 'Entrada',
+    valor: 2650,
+    data: '20/02/2021'
+  },
+  {
+    tipo: 'Saida',
+    valor: 980,
+    data: '20/02/2021'
+  },
+  {
+    tipo: 'Entrada',
+    valor: 20,
+    data: '20/02/2021'
+  },
+  {
+    tipo: 'Saida',
+    valor: 9870,
+    data: '20/02/2021'
+  },
+  {
+    tipo: 'Investimento',
+    valor: 20000,
+    data: '20/02/2021'
+  },
+  {
+    tipo: 'Entrada',
+    valor: 200,
+    data: '20/02/2021'
+  },
+]
   constructor() { }
 
   somavalorTotal(){
-    for(let i = 0; i < this.finances.length; i++){
-      if(this.finances[i].tipo === 'Saída'){
-        this.saida += this.finances[i].valor;
-      }else if(this.finances[i].tipo === 'Entrada'){
-        this.entrada += this.finances[i].valor;
-      }else if(this.finances[i].tipo === 'Investimentos'){
-        this.investimentos += this.finances[i].valor;
+    for(let i = 0; i < this.finance.length; i++){
+      if(this.finance[i].tipo === 'Saída'){
+        this.saida += this.finance[i].valor;
+      }else if(this.finance[i].tipo === 'Entrada'){
+        this.entrada += this.finance[i].valor;
+      }else if(this.finance[i].tipo === 'Investimentos'){
+        this.investimentos += this.finance[i].valor;
+      }
+    }
+    this.saldoConta();
+  }
+  somaSaidas(){
+    var soma = this.finance.map(item => item.valor).reduce((prev, curr) => prev + curr, 0);
+    for(let alt = 0; alt < this.financas.length; alt++){
+      if(this.financas[alt].tipo === 'Saida'){
+        this.saida += this.financas[alt].valor?.valueOf() || 0;
       }
     }
   }
-  saldoConta(){
-    this.valorTotal += this.entrada - this.saida;
+  somaEntradas(){
+    for(let alt = 0; alt < this.financas.length; alt++){
+      if(this.financas[alt].tipo === 'Entrada'){
+        this.entrada += this.financas[alt].valor?.valueOf() || 0;
+      }
+    }
+  }
+  somaInvestimento(){
+    for(let alt = 0; alt < this.financas.length; alt++){
+      if(this.financas[alt].tipo === 'Investimento'){
+        this.investimentos += this.financas[alt].valor?.valueOf() || 0;
+      }
+    }
   }
 
-  excluirItem(id: number | string){
-    this.finances.splice(Number(id), 1)
+  excluir(id: number | string){
+    this.financas.splice(Number(id), 1);
+    console.log("excluir");
     this.valorTotal = 0;
     this.saida = 0;
     this.entrada = 0;
     this.investimentos = 0;
+    this.somaSaidas();
+    this.somaEntradas();
+    this.somaInvestimento();
     this.somavalorTotal();
-    this.saldoConta();
+    this.saveLocalStorage();
+  }
+  saldoConta(){
+    this.valorTotal += this.entrada - this.saida - this.investimentos;
+  }
+
+  createFinance(){
+    this.financas.push({
+       tipo: this.financeModel.tipo,
+       valor: this.financeModel.valor,
+       data: this.financeModel.data
+    });
+    this.recalcular();
+    //this.toastr.success('Financeiro adicionado com sucesso');
+  }
+
+  recalcular(){
+    this.valorTotal = 0;
+    this.saida = 0;
+    this.entrada = 0;
+    this.investimentos = 0;
+    this.somaSaidas();
+    this.somaEntradas();
+    this.somaInvestimento();
+    this.somavalorTotal();
+    this.saveLocalStorage();
+  }
+
+  saveLocalStorage(){
+    if(localStorage.length != 0){
+      if(!localStorage.getItem('local_fuctura')){
+        localStorage.setItem('local_fuctura', JSON.stringify(this.financas));
+    }else{
+      localStorage.removeItem('local_fuctura');
+      localStorage.setItem('local_fuctura', JSON.stringify(this.financas));
+    }
+   }
+  }
+  loadLocalStorage(){
+    if(localStorage.length != 0){
+      if(localStorage.getItem('local_fuctura')){
+        let financas = JSON.parse(localStorage.getItem('local_fuctura'));
+        this.financas = JSON.parse(financas || '');
+      }
+    }
   }
 
 
